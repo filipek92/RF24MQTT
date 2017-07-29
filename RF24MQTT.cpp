dad682ee6f14dded708cf13e8dc64aebbab7ea55
Filip Richter <richter.filip@gmail.com>
2017-07-30 00:10:20 +0200

    Initial commit

---
+++ RF24MQTT.cpp
@@ -0,0 +1,90 @@
+#include "RF24MQTT.h"
+#if defined (__linux) && !defined(__ARDUINO_X86__)
+#include <fstream>
+#endif
+
+RF24MQTT::RF24MQTT( RF24& _radio,RF24Network& _network,RF24Mesh& _mesh): radio(_radio),network(_network),mesh(_mesh){};
+
+bool RF24MQTT::begin(uint8_t channel = MESH_DEFAULT_CHANNEL, rf24_datarate_e data_rate = RF24_1MBPS, uint32_t timeout=MESH_RENEWAL_TIMEOUT ){
+  return true;
+}
+
+uint8_t RF24MQTT::update(){
+  mesh.update();
+  while (network.available()) {
+    RF24NetworkHeader header;
+    char payload[255];
+    char *topic;
+    char *data;
+    uint16_t len = network.read(header, &payload, sizeof(payload));
+    payload[len] = 0;
+    topic = payload;
+    uint16_t presize = strlen(topic) + 1;
+    data = payload + presize;
+    
+    if(callback) callback(topic, data, len-presize);
+  }
+}
+bool RF24MQTT::checkConnection(){
+  RF24NetworkHeader header;
+  uint8_t data[MQTT_MAX_LENGHT]; 
+  if(!mesh.write(NULL, MQTT_PING_TYPE, 0)){
+    if(!mesh.checkConnection()){
+      mesh.renewAddress();
+    }
+    if(!mesh.write(NULL, MQTT_PING_TYPE, 0)) return false;
+  }
+  uint32_t start = millis();
+  while(millis()-start < MQTT_PING_TIMEOUT){
+    if(network.available()){
+      byte len = network.read(header, data, MQTT_MAX_LENGHT);
+      if(header.type == MQTT_PING_TYPE) return true;
+      else proccessMessage(header, data, len);
+    }
+  }
+  return false;  
+}
+
+int RF24MQTT::sendAndReply(unsigned char qtype, unsigned char atype, void* data, byte *length, byte node=0){
+  RF24NetworkHeader header;
+  uint8_t d[MQTT_MAX_LENGHT]; 
+  if(!mesh.write(data, qtype, *length, node)){
+    if(!mesh.checkConnection()){
+      mesh.renewAddress();
+    }
+    if(!mesh.write(data, qtype, *length)) return MQTT_ERROR_MESH_NET;
+  }
+  uint32_t start = millis();
+  while(millis()-start < MQTT_PING_TIMEOUT){  
+    if(network.available()){
+      byte len = network.read(header, d, MQTT_MAX_LENGHT);
+      if(header.type == atype) return 0;
+      else proccessMessage(header, d, len);
+    }
+  }
+  return MQTT_ERROR_TIMEOUT;  
+}
+
+void RF24MQTT::proccessMessage(RF24NetworkHeader& header, void* payload, byte length){
+}
+
+int RF24MQTT::publish(const char topic[], void* payload, byte length, bool retained){
+  uint8_t data[MQTT_MAX_LENGHT];
+  uint8_t topic_len = strlen(topic);
+  strcpy(data, topic);
+  memcpy(data+topic_len+1, payload, length);
+  return mesh.write(data, MQTT_PUBLISH_TYPE, topic_len+length+1);
+}
+
+bool RF24MQTT::subscribe(const char topic[], unsigned char qos = 0){
+  return mesh.write(topic, MQTT_SUBSCRIBE_TYPE, strlen(topic));
+}
+bool RF24MQTT::unsubscribe(const char topic[]){
+  return mesh.write(topic, MQTT_UNSUBSC_TYPE, strlen(topic));
+}
+
+void RF24MQTT::setCallback(mqtt_callback_t _callback){
+  callback = _callback;
+}
+
+byte getTopicFromId(uint16_t topic, char topicName[], byte length); 
---
+++ RF24MQTT.h
@@ -0,0 +1,117 @@
+#ifndef __RF24MQTT_H__
+#define __RF24MQTT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined (__linux) && !defined(__ARDUINO_X86__)
+  #include <RF24/RF24.h>
+  #include <RF24Network/RF24Network.h>
+  #include <RF24Mesh/RF24Mesh.h>
+  #define RF24_LINUX
+#else
+  #include <RF24.h>
+  #include <RF24Network.h>
+  #include <RF24Mesh.h>
+#endif
+
+#define MQTT_PING_TIMEOUT   2000
+#define MQTT_MAX_LENGHT      255
+
+#define MQTT_PING_TYPE      'p'
+#define MQTT_PUBLISH_TYPE   'P'
+#define MQTT_SUBSCRIBE_TYPE 'S'
+#define MQTT_UNSUBSC_TYPE   'U'
+#define MQTT_RECIEVE_TYPE   'R'
+
+#define MQTT_ERROR_TIMEOUT  -1
+#define MQTT_ERROR_MESH_NET -2
+
+typedef void (*mqtt_callback_t)(const char topic[], byte* payload, unsigned int length);
+
+class RF24;
+class RF24Network;
+class RF24Mesh;
+
+class RF24MQTT
+{
+  /**@}*/
+  /**
+   * @name RF24MQTT
+   *
+   *  The mesh library and class documentation is currently in active development and usage may change.
+   */
+  /**@{*/
+  public:
+
+    /**
+   * Construct the mesh:
+   *
+   * @code
+   * RF24 radio(7,8);
+   * RF24Network network(radio);
+   * RF24Mesh mesh(radio,network);
+   * RF24MQTT mqtt(radio,network,mesh);
+   * @endcode
+   * @param _radio The underlying radio driver instance
+   * @param _network The underlying network instance
+   * @param _mesh The underlying mesh instance
+   */
+
+  RF24MQTT(RF24& _radio, RF24Network& _network, RF24Mesh& _mesh);
+
+    /**
+   * Call this in setup() to configure the mesh and request an address.  <br>
+   *  
+   * @code mqtt.begin(); @endcode
+   * This may take a few moments to complete. 
+   * 
+   * The following parameters are optional:
+   * @param channel The radio channel (1-127) default:97
+   * @param data_rate The data rate (RF24_250KBPS,RF24_1MBPS,RF24_2MBPS) default:RF24_1MBPS
+   * @param timeout How long to attempt address renewal in milliseconds default:60000
+   */
+  bool begin(uint8_t channel = MESH_DEFAULT_CHANNEL, rf24_datarate_e data_rate = RF24_1MBPS, uint32_t timeout=MESH_RENEWAL_TIMEOUT );
+
+   /**
+   * Very similar to network.update(), it needs to be called regularly to keep the network
+   * and the mesh going.
+   */   
+  uint8_t update();
+
+   /**
+   * Tests connectivity of this node to the mesh.
+   * @note If this function fails, the radio will be put into standby mode, and will not receive payloads until the address is renewed.
+   * @return Return 1 if connected, 0 if mesh not responding after up to 1 second
+   */
+  
+  bool checkConnection();
+
+  inline int publish(const char topic[], const char payload[])
+  {return publish(topic, payload, strlen(payload), false);}
+  
+  inline int publish(const char topic[], const char payload[], bool retained)
+  {return publish(topic, payload, strlen(payload), retained);}
+  
+  inline int publish(const char topic[], void* payload, byte length)
+  {return publish(topic, payload, length, false);}
+  
+  int publish(const char topic[], void* payload, byte length, bool retained);
+
+
+  bool subscribe(const char topic[], unsigned char qos = 0);
+  bool unsubscribe(const char topic[]); 
+
+  void setCallback(mqtt_callback_t _callback);
+
+  private:
+  void proccessMessage(RF24NetworkHeader& header, void* payload, byte length);
+  int RF24MQTT::sendAndReply(unsigned char qtype, unsigned char atype, void* data, byte *length, byte node=0);
+
+  mqtt_callback_t callback;
+  
+  RF24& radio;
+  RF24Network& network;
+  RF24Mesh& mesh;
+};
+#endif
